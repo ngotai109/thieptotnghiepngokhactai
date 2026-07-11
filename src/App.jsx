044dfde64815143dcf020e94abb88b691b840c06
ngotai109 <glucozo109@gmail.com>
2026-07-11 17:01:15 +0700

    Add Google Maps iframe for event location

--- src/App.jsx
+++ src/App.jsx
@@ -169,6 +169,18 @@ function App() {
               </div>
             </motion.div>
 
+            <motion.div className="map-container" variants={itemVariants}>
+              <iframe 
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3723.4736632151066!2d105.73252651119284!3d21.053735986838664!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x31345457e292d5bf%3A0x20ac91c94d74439a!2zVHLGsOG7nW5nIMSQ4bqhaSBo4buNYyBDw7RuZyBuZ2hp4buHcCBIw6AgTuG7mWk!5e0!3m2!1svi!2s!4v1783763989086!5m2!1svi!2s" 
+                width="100%" 
+                height="200" 
+                style={{ border: 0 }} 
+                allowFullScreen="" 
+                loading="lazy" 
+                referrerPolicy="strict-origin-when-cross-origin">
+              </iframe>
+            </motion.div>
+
             <motion.button 
               className="rsvp-btn"
               variants={itemVariants}
